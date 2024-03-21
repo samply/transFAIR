56fb2557ae6e2704278d7968db05176d209e135f
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2024-03-21 16:05:32 +0100

    build: added basic rust build
    use nix build, nix run and nix develop

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "wip-routine-connector";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay}:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          name = "wip-routine-connector";
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          nativeBuildInputs = with pkgs; [ rustToolchain pkg-config ];
+          buildInputs = with pkgs; [ openssl ];
+        in
+        rec {
+          # nix build
+          packages.${name} = pkgs.rustPlatform.buildRustPackage {
+            inherit name;
+            src = ./.;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+          };
+          defaultPackage = packages.${name};
+
+          # nix run
+          apps.${name} = flake-utils.lib.mkApp {
+            inherit name;
+            drv = packages.${name};
+          };
+          defaultApp = apps.${name};
+
+          # nix develop
+          devShells.default = pkgs.mkShell {
+            inherit buildInputs nativeBuildInputs;
+          };
+        }
+      );
+}
