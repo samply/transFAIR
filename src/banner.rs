d2bf462a803b36a1705bc7a06c068ef5be40bdc3
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2024-03-26 14:02:27 +0100

    refactor: include git info in banner

--- src/banner.rs
+++ src/banner.rs
@@ -1,10 +1,17 @@
 use tracing::info;
 
 pub(crate) fn print_banner() {
+    let commit = match env!("GIT_DIRTY") {
+        "false" => {
+            env!("GIT_COMMIT_SHORT")
+        }
+        _ => "SNAPSHOT",
+    };
     info!(
-        ":newspaper: Routine Connector v{} (built {} {}) starting up ...",
+        "Routine Connector v{} (built {} {}, {}) starting up ...",
         env!("CARGO_PKG_VERSION"),
         env!("BUILD_DATE"),
         env!("BUILD_TIME"),
+        commit
     );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use tracing::Level;
+use tracing_subscriber::{EnvFilter, util::SubscriberInitExt};
+
 mod banner;
 
 fn main() {
+    tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(Level::DEBUG)
+        .with_env_filter(EnvFilter::from_default_env())
+        .finish()
+        .init();
     banner::print_banner();
 }
