0dcca315e7807b2e21092162cf80b560b7183b07
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2024-04-26 13:43:00 +0200

    feat: implemented create project pseudonym

--- src/mainzelliste.rs
+++ src/mainzelliste.rs
@@ -1,25 +1,116 @@
 // Client implementation for Mainzelliste TTP
 
+use std::str::FromStr;
+
+use fhir_sdk::r4b::{
+    codes::AdministrativeGender,
+    resources::Patient,
+    types::{HumanName, Identifier},
+};
 use reqwest::StatusCode;
-use tracing::warn;
+use tracing::{debug, warn};
 
 use crate::CONFIG;
 
 pub async fn get_supported_ids() -> Result<Vec<String>, (StatusCode, &'static str)> {
-    let idtypes_endpoint = CONFIG.institute_ttp_url.join("configuration/idTypes").unwrap();
-    let supported_ids = reqwest::Client::new().get(idtypes_endpoint)
+    let idtypes_endpoint = CONFIG
+        .institute_ttp_url
+        .join("configuration/idTypes")
+        .unwrap();
+    let supported_ids = reqwest::Client::new()
+        .get(idtypes_endpoint)
         .header("mainzellisteApiKey", &CONFIG.institute_ttp_api_key)
         .send()
         .await
         .map_err(|err| {
-            warn!("Couldn't connect to Mainzelliste. Request failed with error: {}", err);
+            warn!(
+                "Couldn't connect to Mainzelliste. Request failed with error: {}",
+                err
+            );
             (StatusCode::SERVICE_UNAVAILABLE, "Connection to TTP failed.")
         })?
         .json::<Vec<String>>()
         .await
         .map_err(|err| {
-            warn!("Failed to parse returned idTypes from Mainzelliste. Failed with error: {}", err);
-            (StatusCode::SERVICE_UNAVAILABLE, "Unable to parse Mainzelliste response as JSON")
+            warn!(
+                "Failed to parse returned idTypes from Mainzelliste. Failed with error: {}",
+                err
+            );
+            (
+                StatusCode::SERVICE_UNAVAILABLE,
+                "Unable to parse Mainzelliste response as JSON",
+            )
         })?;
     Ok(supported_ids)
+}
+
+pub async fn create_project_pseudonym(
+    patient: crate::requests::Patient,
+) -> Result<Vec<Option<Identifier>>, (StatusCode, &'static str)> {
+    let identifier_request = build_identifier_request(patient);
+
+    let patients_endpoint = CONFIG
+        .institute_ttp_url
+        .join("fhir/Patient")
+        .unwrap();
+
+    let response = reqwest::Client::new()
+        .post(patients_endpoint)
+        .header("mainzellisteApiKey", CONFIG.institute_ttp_api_key.clone())
+        .json(&identifier_request)
+        .send()
+        .await
+        .map_err(|err| 
+            (StatusCode::SERVICE_UNAVAILABLE, "Failed to communicate with mainzelliste: {}", err))
+            .unwrap();
+    
+    let patient = response.json::<Patient>().await.map_err(|err| 
+        (StatusCode::INTERNAL_SERVER_ERROR, "Couldn't parse mainzelliste response as json: {}", err)).unwrap();
+    
+    let patient_identifiers = patient.identifier.clone();
+
+    Ok(patient_identifiers)
+}
+
+// maps the program input to a valid identifier request in fhir
+fn build_identifier_request(patient: crate::requests::Patient) -> Patient {
+     let identifier_requests = patient
+        .identifiers
+        .iter()
+        .map(|identifier| {
+            Some(
+                Identifier::builder()
+                    .r#use(fhir_sdk::r4b::codes::IdentifierUse::Temp)
+                    .system(identifier.to_owned())
+                    .build()
+                    .unwrap(),
+            )
+        })
+        .collect();
+
+    let birth_date = fhir_sdk::Date::from_str(
+        &patient.birth_date.to_string()
+    ).map_err(|err| {
+        warn!("Couln't parse birthdate from input. Error war {}", err);
+        (StatusCode::BAD_REQUEST, "Couln't parse birthdate from input.")
+    }).unwrap();
+
+    let patient = Patient::builder()
+        .active(false)
+        .identifier(identifier_requests)
+        .gender(AdministrativeGender::Male)
+        .name(vec![Some(
+            HumanName::builder()
+                .r#use(fhir_sdk::r4b::codes::NameUse::Official)
+                .family(patient.name.family)
+                .given(vec![Some(patient.name.given)])
+                .build()
+                .unwrap(),
+        )])
+        .birth_date(birth_date)
+        .build()
+        .unwrap();
+
+    debug!("Created following patient resource: {:?}", patient);
+    patient
 }
--- src/requests.rs
+++ src/requests.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use axum::{extract::Path, Json};
 use chrono::NaiveDate;
 use reqwest::StatusCode;
@@ -7,7 +5,7 @@ use serde::{Serialize, Deserialize};
 use tracing::debug;
 use uuid::Uuid;
 
-use crate::mainzelliste::get_supported_ids;
+use crate::mainzelliste::{create_project_pseudonym, get_supported_ids};
 
 #[derive(Serialize)]
 pub enum RequestStatus {
@@ -23,30 +21,31 @@ pub struct DataRequest {
     status: RequestStatus,
 }
 
-#[derive(Serialize, Deserialize)]
-struct Name {
-    family: String,
-    given: String,
-    prefix: Vec<String>
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Name {
+    pub family: String,
+    pub given: String,
+    pub prefix: Vec<String>
 }
 
 type IdType = String;
-type Identifier = String;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Patient {
-    name: Name,
-    birth_date: NaiveDate,
-    identifiers: HashMap<IdType, Identifier>
+    pub name: Name,
+    pub birth_date: NaiveDate,
+    pub identifiers: Vec<IdType>
 }
 
 // POST /requests; Creates a new Data Request
 pub async fn create_data_request(Json(patient): Json<Patient>) -> Result<Json<DataRequest>, (StatusCode, &'static str)> {
-    validate_data_request(patient).await?;
+    validate_data_request(patient.clone()).await?;
     let data_request = DataRequest {
       id: Uuid::new_v4(),
-      status: RequestStatus::Error,
+      status: RequestStatus::Created,
     };
+    let identifiers = create_project_pseudonym(patient.clone()).await?;
+    debug!("TTP Returned these identifiers {:#?}", identifiers);
     Ok(Json(data_request))
 }
 
@@ -73,7 +72,8 @@ pub async fn get_data_request(Path(request_id): Path<Uuid>) -> Json<DataRequest>
 
 async fn validate_data_request(patient: Patient) -> Result<(), (StatusCode, &'static str)>  {
     let ttp_supported_ids = get_supported_ids().await?;
-    let are_ids_supported = patient.identifiers.keys()
+    let are_ids_supported = patient.identifiers
+            .iter()
             .all(|identifier| ttp_supported_ids.contains(identifier));
     if ! are_ids_supported {
         Err((StatusCode::BAD_REQUEST, "The TTP doesn't support one of the requested id types."))
