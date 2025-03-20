b77144df3b9881ebd30830317bd1b217e4cef77c
David Croft <d.croft@dkfz-heidelberg.de>
2025-03-20 10:33:53 +0100

    Insert patient birth date
    Patient birth date was not present in the previous iteration of this software
    and that led to problems with Directory sync.
    
    This was fixed by combining the patient age at earliest sample with the
    sample collection dates.

--- src/main/java/de/samply/transfair/reader/FhirCrcReader.java
+++ src/main/java/de/samply/transfair/reader/FhirCrcReader.java
@@ -13,11 +13,18 @@ import org.springframework.beans.factory.annotation.Value;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Specimen;
 import org.hl7.fhir.r4.model.Organization;
+import org.hl7.fhir.r4.model.Resource;
+import org.hl7.fhir.r4.model.Extension;
+import org.hl7.fhir.r4.model.Type;
+import org.hl7.fhir.r4.model.IntegerType;
+import org.hl7.fhir.r4.model.StringType;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashMap;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Map;
 
 /**
@@ -65,6 +72,7 @@ public class FhirCrcReader implements ItemReader<Bundle> {
    * @return
    */
   public Bundle read() {
+    log.info("read: entered");
     if (bundle == null)
       bundle = new Bundle();
     else
@@ -75,6 +83,7 @@ public class FhirCrcReader implements ItemReader<Bundle> {
     CollectionBuilder collectionBuilder = new CollectionBuilder();
     BiobankBuilder biobankBuilder = new BiobankBuilder();
     ConditionBuilder conditionBuilder = new ConditionBuilder();
+    log.info("read: reading patient data from: " + patientFilePath);
     for (Map<String, String> record : CsvReader.readCsvFilesInDirectory(patientFilePath)) {
       log.info("read: patient record: " + JsonSerializer.toJsonString(record));
       Patient patient = patientBuilder.build(record, patientMaxCount);
@@ -82,18 +91,130 @@ public class FhirCrcReader implements ItemReader<Bundle> {
         break;
       conditionBuilder.build(patient, record);
     }
+    log.info("read: reading specimen data from: " + specimenFilePath);
     for (Map<String, String> record : CsvReader.readCsvFilesInDirectory(specimenFilePath)) {
       Organization biobank = biobankBuilder.build(record);
       Organization collection = collectionBuilder.build(biobank, record);
       specimenBuilder.build(patientBuilder, collection, record);
     }
 
+    calculatePatientDatesOfBirth(patientBuilder, specimenBuilder);
+
+    log.info("read: adding resources to bundle");
     patientBuilder.addResourcesToBundle(bundle);
     specimenBuilder.addResourcesToBundle(bundle);
     collectionBuilder.addResourcesToBundle(bundle);
     biobankBuilder.addResourcesToBundle(bundle);
     conditionBuilder.addResourcesToBundle(bundle);
 
+    log.info("read: done, bundle size: " + bundle.getEntry().size());
     return bundle;
+  }
+
+  /**
+   * Calculates the date of birth of each Patient based on the data in the Specimen table.
+   *
+   * The Patient objects contain an extension with patient age at first diagnosis, in years.
+   *
+   * Specimen objects contain a collection year.
+   *
+   * This method finds the earliest collection year in the Specimen table for each Patient
+   * and subtracts the patient age to get the year of birth.
+   *
+   *
+   * @param patientBuilder
+   * @param specimenBuilder
+   */
+  private void calculatePatientDatesOfBirth(PatientBuilder patientBuilder, SpecimenBuilder specimenBuilder) {
+    Map<String, Resource> patientMap = patientBuilder.getResourceMap();
+    Map<String, Resource> specimenMap = specimenBuilder.getResourceMap();
+    int patientCount = 0;
+    int ageExtensionCount = 0;
+    int patientAgeCount = 0;
+    int specimenAvailableCount = 0;
+    int specimenDateTimeCount = 0;
+    for (Resource patient : patientMap.values()) {
+      patientCount++;
+      Patient patientObj = (Patient) patient;
+
+      // Get the extension
+      Extension ageExtension = patientObj.getExtensionByUrl("https://bbmri.crc/fhir/StructureDefinition/PatientAge");
+      if (ageExtension == null) {
+        log.warn("calculatePatientDatesOfBirth: Extension https://bbmri.crc/fhir/StructureDefinition/PatientAge not found");
+        continue;
+      }
+      ageExtensionCount++;
+      // Get the value from the extension
+      Type ageValue = ageExtension.getValue();
+      if (ageValue == null) {
+        log.warn("calculatePatientDatesOfBirth: Value not found in extension https://bbmri.crc/fhir/StructureDefinition/PatientAge");
+        continue;
+      }
+      // Cast to IntegerType
+      int patientAge = (-1);
+      if (ageValue instanceof IntegerType)
+        patientAge = ((IntegerType) ageValue).getValue();
+      else {
+        String stringAgeValue = ageValue.toString();
+        if (stringAgeValue == null) {
+          log.warn("calculatePatientDatesOfBirth: stringAgeValue == null");
+          continue;
+        } else if (stringAgeValue.isEmpty()) {
+          log.warn("calculatePatientDatesOfBirth: stringAgeValue.isEmpty()");
+          continue;
+        } else if (!stringAgeValue.matches("\\d+")) {
+          log.warn("calculatePatientDatesOfBirth: stringAgeValue does not not look like an integer: " + stringAgeValue);
+          continue;
+        }
+        patientAge = Integer.parseInt(stringAgeValue);
+      }
+      patientAgeCount++;
+      int minYear = 10000;
+      boolean specimenAvailable = false;
+      boolean specimenHasCollectedDateTime = false;
+      for (Resource specimen : specimenMap.values()) {
+        if (((Specimen)specimen).hasSubject() && ((Specimen)specimen).getSubject().getReference().equals("Patient/" + patientObj.getIdElement().getIdPart())) {
+          specimenAvailable = true;
+          if (((Specimen) specimen).hasCollection() && ((Specimen) specimen).getCollection().hasCollectedDateTimeType()) {
+            specimenHasCollectedDateTime = true;
+            Date collectedDate = ((Specimen) specimen).getCollection().getCollectedDateTimeType().getValue();
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(collectedDate);
+            int year = calendar.get(Calendar.YEAR);
+            if (year < minYear)
+              minYear = year;
+          }
+        }
+      }
+      if (!specimenAvailable) {
+        log.warn("calculatePatientDatesOfBirth: no specimens available for patient: " + patientObj.getIdElement().getIdPart());
+        log.warn("calculatePatientDatesOfBirth: patient identifier: " + patientObj.getIdentifier().get(0).getValue());
+        continue;
+      } else
+        specimenAvailableCount++;
+      if (!specimenHasCollectedDateTime) {
+        log.warn("calculatePatientDatesOfBirth: no specimens with collection date for patient: " + patientObj.getIdElement().getIdPart());
+        continue;
+      } else
+        specimenDateTimeCount++;
+
+      if (minYear < 10000) {
+        int yearOfBirth = minYear - patientAge;
+        // Set the date to January 1st of the calculated birth year
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear(); // Clear all fields to avoid unwanted time parts
+        calendar.set(Calendar.YEAR, yearOfBirth);
+        calendar.set(Calendar.MONTH, Calendar.JANUARY);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+
+        // Set the birth date in the Patient object
+        patientObj.getBirthDateElement().setValue(calendar.getTime());
+      }
+    }
+    log.info("calculatePatientDatesOfBirth: patientCount: " + patientCount);
+    log.info("calculatePatientDatesOfBirth: ageExtensionCount: " + ageExtensionCount);
+    log.info("calculatePatientDatesOfBirth: patientAgeCount: " + patientAgeCount);
+    log.info("calculatePatientDatesOfBirth: specimenAvailableCount: " + specimenAvailableCount);
+    log.info("calculatePatientDatesOfBirth: specimenDateTimeCount: " + specimenDateTimeCount);
   }
 }
--- src/main/java/de/samply/transfair/reader/crc/ResourceBuilder.java
+++ src/main/java/de/samply/transfair/reader/crc/ResourceBuilder.java
@@ -7,7 +7,6 @@ import org.hl7.fhir.r4.model.Resource;
 import org.hl7.fhir.r4.model.StringType;
 import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.instance.model.api.IBaseResource;
-//import org.hl7.fhir.instance.model.api.IBaseHasIdentifier;
 import org.hl7.fhir.r4.model.DomainResource;
 import org.hl7.fhir.r4.model.Patient;
 import org.hl7.fhir.r4.model.Observation;
@@ -19,14 +18,42 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+/**
+ * Abstract base class for building and managing FHIR resources.
+ *
+ * <p>This class provides common utility methods for handling FHIR resources,
+ * including resource identification, extension creation, and adding resources to a bundle.
+ */
 @Slf4j
 public abstract class ResourceBuilder {
+    // Resource cache, to avoid generating resources repeatedly.
     protected Map<String, Resource> resourceMap = new HashMap<String, Resource>();
 
+    /**
+     * Retrieves the number of resources currently stored in the resource map.
+     *
+     * @return The number of resources in {@code resourceMap}.
+     */
     protected int getResourceMapSize() {
         return resourceMap.size();
     }
 
+    /**
+     * Returns the resource map containing all cached resources.
+     *
+     * @return A map where keys are resource identifiers and values are {@link Resource} objects.
+     */
+    public Map<String, Resource> getResourceMap() {
+        return resourceMap;
+    }
+
+    /**
+     * Retrieves the ID of a resource from the resource map based on a given key
+     * (e.g. resource ID).
+     *
+     * @param key The key associated with the resource.
+     * @return The resource ID if found, otherwise {@code null}.
+     */
     public String getResourceId(String key) {
         if (key == null) {
             log.warn("getResourceId: Key is null");
@@ -37,15 +64,27 @@ public abstract class ResourceBuilder {
         return resourceMap.get(key).getIdElement().getIdPart();
     }
 
+    /**
+     * Adds all stored resources to the supplied FHIR {@link Bundle}.
+     *
+     * @param bundle The {@link Bundle} to which resources should be added.
+     */
     public void addResourcesToBundle(Bundle bundle) {
         for (Resource resource : resourceMap.values()) {
-            log.info("Adding to bundle: " + resource.getIdElement().getIdPart());
             Bundle.BundleEntryComponent entry = new Bundle.BundleEntryComponent();
             entry.setResource(resource);
             bundle.addEntry(entry);
         }
+        log.info("Bundle size: " + bundle.getEntry().size());
     }
 
+    /**
+     * Creates a FHIR {@link Extension} with a string value.
+     *
+     * @param value The string value for the extension.
+     * @param url   The URL defining the extension.
+     * @return A newly created {@link Extension} with the given value and URL.
+     */
     protected Extension createStringExtension(String value, String url) {
         Extension extension = new Extension();
         extension.setUrl(url);
@@ -54,10 +93,26 @@ public abstract class ResourceBuilder {
         return extension;
     }
 
+    /**
+     * Assigns a random UUID as the ID of the given resource.
+     *
+     * @param resource The {@link Resource} to assign an ID.
+     */
     protected void setId(Resource resource) {
         resource.setId(UUID.randomUUID().toString());
     }
 
+    /**
+     * Sets an identifier for a given {@link DomainResource}.
+     *
+     * <p>The identifier is assigned based on the BBMRI-ERIC system.
+     * This method only supports specific resource types such as {@link Patient},
+     * {@link Observation}, {@link Condition}, {@link Organization}, and {@link Specimen}.
+     * If an unsupported resource type is provided, a warning is logged.
+     *
+     * @param resource         The {@link DomainResource} to assign an identifier.
+     * @param identifierString The identifier value to set.
+     */
     protected void setIdentifier(DomainResource resource, String identifierString) {
         Identifier identifier = new Identifier()
                 .setSystem("http://www.bbmri-eric.eu/")
@@ -79,10 +134,28 @@ public abstract class ResourceBuilder {
         }
     }
 
+    /**
+     * Generates a resource identifier from a given record.
+     *
+     * <p>This method looks for a key named "id" in the record and calls
+     * {@link #generateResourceIdentifier(String)} if found.
+     *
+     * @param record A map containing key-value pairs representing resource data.
+     * @return A generated resource identifier, or {@code null} if the key is missing.
+     */
     protected String generateResourceIdentifier(Map<String, String> record) {
         return generateResourceIdentifier(record, "id");
     }
 
+    /**
+     * Generates a resource identifier from a given record using a specific key.
+     *
+     * <p>If the key does not exist in the record, a warning is logged.
+     *
+     * @param record A map containing key-value pairs representing resource data.
+     * @param key    The key whose value will be used as the resource identifier.
+     * @return A generated resource identifier, or {@code null} if the key is missing.
+     */
     protected String generateResourceIdentifier(Map<String, String> record, String key) {
         if (!record.containsKey(key)) {
             log.warn("generateResourceIdentifier: " + key + " not found in record. Skipping.");
@@ -91,6 +164,16 @@ public abstract class ResourceBuilder {
         return generateResourceIdentifier(record.get(key));
     }
 
+    /**
+     * Generates a resource identifier from an input string.
+     *
+     * This default implementation simply returns the input string.
+     *
+     * <p>This method can be overridden by subclasses to implement custom identifier generation logic.
+     *
+     * @param input The input string from which the identifier will be generated.
+     * @return The generated identifier.
+     */
     protected String generateResourceIdentifier(String input) {
         return input;
     }
